56b6ce385a41cfc4b28d81c58cb75e68d922a977
Ruslan <ruslan.lantsov.m@gmail.com>
2025-11-10 20:45:20 +0200

    adds colors

---
+++ ThreadsClone/Core/Common/Colors.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+final class Colors {
+    static let background = Color("background")
+    static let title = Color("title")
+    static let subtitle = Color("subtitle")
+    static let formBorder = Color("formBorder")
+    static let buttonBorder = Color("buttonBorder")
+    static let divier = Color("divier")
+    static let link = Color("link")
+    static let threadGrey = Color("threadGrey")
+    static let threadOptionBg = Color("threadOptionBg")
+    static let threadOptionCard = Color("threadOptionCard")
+}
