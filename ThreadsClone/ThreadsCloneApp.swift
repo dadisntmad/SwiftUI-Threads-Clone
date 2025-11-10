0715d7df67dc7ca0bba2436b523cf2008b181f7d
Ruslan <ruslan.lantsov.m@gmail.com>
2025-11-10 20:06:06 +0200

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Ruslan Lantsov on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
