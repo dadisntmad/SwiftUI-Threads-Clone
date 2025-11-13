68068b85e293328dcbfa70754c4b020013a449a0
Ruslan <ruslan.lantsov.m@gmail.com>
2025-11-13 17:52:34 +0200

    creates bottom navigation bar

---
+++ ThreadsClone/Core/Views/Main/View/MainView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct MainView: View {
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            Tab("", image: selectedTab == 0 ? Icons.feedFilled : Icons.feed, value: 0) {
+                Text("Home View")
+            }
+            
+            Tab("", image: selectedTab == 1 ? Icons.exploreFilled : Icons.explore, value: 1) {
+                Text("Explore View")
+            }
+            
+            Tab("", image: selectedTab == 2 ? Icons.writeFilled : Icons.write, value: 2) {
+                Text("Write View")
+            }
+            
+            Tab("", image: selectedTab == 3 ? Icons.heartFilled : Icons.heart, value: 3) {
+                Text("Notifications View")
+            }
+            
+            Tab("", image: selectedTab == 4 ? Icons.profileFilled : Icons.profile, value: 4) {
+                Text("Profile View")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
