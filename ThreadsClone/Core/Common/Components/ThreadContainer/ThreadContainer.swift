b66e99aaae43d9573f2c0ed5f95942284faae1b5
Ruslan <ruslan.lantsov.m@gmail.com>
2025-11-18 17:45:59 +0200

    adds thread images

--- ThreadsClone/Core/Common/Components/ThreadContainer/ThreadContainer.swift
+++ ThreadsClone/Core/Common/Components/ThreadContainer/ThreadContainer.swift
@@ -1,6 +1,13 @@
 import SwiftUI
+import Kingfisher
 
 struct ThreadContainer: View {
+    private var images = [
+        "https://images.pexels.com/photos/34629969/pexels-photo-34629969.jpeg",
+        "https://images.pexels.com/photos/15112647/pexels-photo-15112647.jpeg",
+        "https://images.pexels.com/photos/30097108/pexels-photo-30097108.jpeg"
+    ]
+    
     var body: some View {
         HStack(alignment: .top, spacing: 10) {
             ProfileImage(
@@ -35,6 +42,25 @@ struct ThreadContainer: View {
                 Text("Let's talk about the incredible power of perseverance and how it can change your life. 🚀 ")
                     .font(.body)
                     .padding(.bottom)
+                
+                if !images.isEmpty {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(images, id: \.self) { imageUrl in
+                                KFImage(URL(string: imageUrl))
+                                    .resizable()
+                                    .aspectRatio(1.0, contentMode: .fill)
+                                    .frame(width: 250, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                                    .clipped()
+
+                            }
+                        }
+                    }
+                    .frame(height: 300)
+                    .scrollTargetBehavior(.paging)
+                    .padding([.trailing, .bottom])
+                }
                 
                 // action buttons
                 HStack(spacing: 18) {
